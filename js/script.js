71ed5207cf9ea1efed80659fdf6ece50d0113200
florentsahiti <florentsahiti06@gmail.com>
2022-02-18 01:41:08 +0100

    link fixed

--- js/script.js
+++ js/script.js
@@ -71,3 +71,5 @@ allLinks.forEach(function (link) {
 
 //   updateSlidePosition();
 // }
+
+jQuery(document).ready(function () { var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor); if (window.location.hash && isChrome) { setTimeout(function () { var hash = window.location.hash; window.location.hash = “”; window.location.hash = hash; }, 300); } });
